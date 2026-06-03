c0a5b5472b4a78622252678b9fe70a481d1015bd
VishalKumarTrayistats <vishal.kumar@trayistats.com>
2026-06-03 16:00:43 +0530

    PokemonCharacterDataFetching
    Implemented redux toolkit in online pokemon characted data fetching web app.

---
+++ ReduxProject/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchPokemon } from "./features/pokemon/pokemonSlice";
+
+function App() {
+  const [pokemonName, setPokemonName] =
+    useState("");
+
+  const dispatch = useDispatch();
+
+  const { pokemon, loading, error } =
+    useSelector((state) => state.pokemon);
+
+  const handleSearch = () => {
+    dispatch(fetchPokemon(pokemonName));
+  };
+
+  return (
+    <div>
+      <h1>Pokemon Search</h1>
+
+      <input
+        type="text"
+        placeholder="Enter pokemon name"
+        value={pokemonName}
+        onChange={(e) =>
+          setPokemonName(e.target.value)
+        }
+      />
+
+      <button onClick={handleSearch}>
+        Search
+      </button>
+
+      {loading && <h2>Loading...</h2>}
+
+      {error && <h2>{error}</h2>}
+
+      {pokemon && (
+        <div>
+          <h2>{pokemon.name}</h2>
+
+          <p>ID: {pokemon.id}</p>
+
+          <p>Height: {pokemon.height}</p>
+
+          <p>Weight: {pokemon.weight}</p>
+
+          <img
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
